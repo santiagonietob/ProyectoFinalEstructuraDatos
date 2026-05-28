31261c4c6690bf75dfed009952f86a009ccd1e09
Mauro251006 <andresm.perdomoc@uqvirtual.edu.co>
2026-05-28 15:27:18 -0500

    Zona Horaria

--- billetera/src/main/java/com/fintech/billetera/BilleteraApplication.java
+++ billetera/src/main/java/com/fintech/billetera/BilleteraApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BilleteraApplication {
 
 	public static void main(String[] args) {
+		java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("America/Bogota"));
 		SpringApplication.run(BilleteraApplication.class, args);
 	}
 
